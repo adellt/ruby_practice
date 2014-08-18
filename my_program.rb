ebaeaf7f3c6f5bffdb3a93b8b7919361598988a9
adellt <adell568@gmail.com>
2014-08-18 17:36:11 +1000

    Start

---
+++ my_program.rb
@@ -0,0 +1,20 @@
+class Sample
+	def hello
+		puts "Hello, world!"
+	end
+end
+
+s = Sample.new
+s.hello
+
+def water_status(minutes)
+	if minutes < 7
+		puts "The wwater is not boiling yet."
+	elsif minutes == 7
+		puts "It's just barely boiling."
+	elsif minutes == 8
+		puts "It's boiling!"
+	else 
+		puts "Hot! Hot! Hot!"
+	end
+end
